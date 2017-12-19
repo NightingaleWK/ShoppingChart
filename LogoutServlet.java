165f878672206ff167c810b5f4d7c64fc944babe
WK <33059842+NightingaleWK@users.noreply.github.com>
2017-12-19 20:25:27 +0800

    Create LogoutServlet.java

---
+++ LogoutServlet.java
@@ -0,0 +1,16 @@
+package wangkai;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+public class LogoutServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+         // 将Session对象中的User对象移除
+		request.getSession().removeAttribute("user");
+		response.sendRedirect("/wangkai/IndexServlet");
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
